571af4bafdd7c5613666a725861f33955f336aaa
Tom Everett <tom@khubla.com>
2019-01-06 15:25:35 -0700

    date and time fun

--- src/test/java/test/com/khubla/pdxreader/util/TestParadoxDate.java
+++ src/test/java/test/com/khubla/pdxreader/util/TestParadoxDate.java
@@ -10,6 +10,9 @@ import com.khubla.pdxreader.util.ParadoxDate;
 
 @Test
 public class TestParadoxDate {
+   /**
+    * date 0 Jan 1, 1 A.D.
+    */
    public void testDate1() {
       try {
          Date date = ParadoxDate.getDateFromParadoxDate(0);
@@ -24,7 +27,7 @@ public class TestParadoxDate {
       }
    }
 
-   /*
+   /**
     * 100 days from January 1, 1 A.D is April 11, 1 A.D.
     */
    public void testDate2() {
@@ -35,6 +38,23 @@ public class TestParadoxDate {
          Assert.assertTrue(calendar.get(Calendar.DATE) == 12);
          Assert.assertTrue(calendar.get(Calendar.MONTH) == 4);
          Assert.assertTrue(calendar.get(Calendar.YEAR) == 1);
+      } catch (final Exception e) {
+         e.printStackTrace();
+         Assert.fail();
+      }
+   }
+
+   /**
+    * 2944 days from January 1, 1 A.D is Jan 23, 9 A.D.
+    */
+   public void testDate3() {
+      try {
+         Date date = ParadoxDate.getDateFromParadoxDate(2944);
+         Calendar calendar = Calendar.getInstance();
+         calendar.setTime(date);
+         Assert.assertTrue(calendar.get(Calendar.DATE) == 23);
+         Assert.assertTrue(calendar.get(Calendar.MONTH) == 1);
+         Assert.assertTrue(calendar.get(Calendar.YEAR) == 9);
       } catch (final Exception e) {
          e.printStackTrace();
          Assert.fail();
